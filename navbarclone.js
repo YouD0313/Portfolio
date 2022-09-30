577530d6b7b82d39857b277f4d0e002e2f0a345a
YouD0313 <102004480+YouD0313@users.noreply.github.com>
2022-09-30 20:35:18 +0900

    js click, html certificate img
    navbar click scroll event

--- navbarclone.js
+++ navbarclone.js
@@ -11,3 +11,28 @@ document.addEventListener('scroll', () => {
 		navbar.classList.remove('navbar--change');
 	}
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+	// console.log(event.target.dataset.link);
+	const target = event.target;
+	const link = target.dataset.link;
+	// console.log(target.link);
+	if (link == null) {
+		return;
+	}
+
+	scrollIntoView(link);
+});
+
+// Handle click on "contact me" button on home
+const btnContact = document.querySelector('.home__contact');
+btnContact.addEventListener('click', () => {
+	scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+	const contact = document.querySelector(selector);
+	contact.scrollIntoView({ behavior: 'smooth' });
+}
