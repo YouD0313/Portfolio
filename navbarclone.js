b381a67a6d5e4e49a9d2b0e3e3074831f272fc7f
YouD0313 <102004480+YouD0313@users.noreply.github.com>
2022-10-04 15:00:47 +0900

    js scroll navbar--change if >= 1

--- navbarclone.js
+++ navbarclone.js
@@ -1,11 +1,11 @@
 // window.scrollY 가 navbarHeight 보다 작을 때 투명하게
 const navbar = document.getElementById('navbar');
 // navbar의 height 구하는 함수
-const navbarHegiht = navbar.getBoundingClientRect().height;
+const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
 	// console.log(window.scrollY);
-	// console.log(`height : ${navbarHegiht}`);
-	if (window.scrollY >= navbarHegiht) {
+	// console.log(`height : ${navbarHeight}`);
+	if (window.scrollY >= 1) {
 		navbar.classList.add('navbar--change');
 	} else {
 		navbar.classList.remove('navbar--change');
